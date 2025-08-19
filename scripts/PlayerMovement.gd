29b2986af3dc08a368dcc402ec8778aba9871804
AlexD717 <alex717nd@gmail.com>
2025-08-18 18:50:14 -0700

    Cayote Time

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,22 +1,33 @@
 extends CharacterBody2D
 
+@export var SPEED: int = 500
+@export var JUMP_VELOCITY: int = -450
+@export var CAYOTE_TIME: float = 100
 
-@export var SPEED = 500.0
-@export var JUMP_VELOCITY = -450.0
+var lastGroundedTime: int = 0
+var isGrounded: bool = false
 
+func CanJump() -> bool:
+	return isGrounded || (Time.get_ticks_msec() - lastGroundedTime < CAYOTE_TIME)
 
 func _physics_process(delta: float) -> void:
+	# check if grounded
+	isGrounded = is_on_floor()
+
 	# Add the gravity.
-	if not is_on_floor():
+	if not isGrounded:
 		velocity += get_gravity() * delta
+	# Set last ground time
+	else:
+		lastGroundedTime = Time.get_ticks_msec()
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and CanJump():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
