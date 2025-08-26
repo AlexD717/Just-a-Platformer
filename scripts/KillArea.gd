ab61776a315753382905ed486819e9cb908f567f
Alexey Dmitriev <alex717nd@gmail.com>
2025-08-26 13:10:57 -0700

    Spike Traps & Player Death Effect

--- scripts/KillArea.gd
+++ scripts/KillArea.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
-@export var killDelay: float = 0.5
+@export var doKillAnimation: bool = true
 
-func _on_body_entered(body: Node2D) -> void:
+func _ready() -> void:
+	body_entered.connect(bodyEntered)
+
+func bodyEntered(body: Node2D) -> void:
 	if (body.is_in_group("Player")):
 		killBody(body)
 
 func killBody(body: Node2D) -> void:
 	if (!body): return
-	await get_tree().create_timer(killDelay).timeout
+	# Calls playerDied() on the PlayerHealth node
 	for child: Node in body.get_children():
 		if (child is PlayerHealth):
-			child.playerDied()
+			child.playerDied(doKillAnimation)
 			return
--- scripts/PlayerHealth.gd
+++ scripts/PlayerHealth.gd
@@ -1,7 +1,22 @@
 extends Node2D
 class_name PlayerHealth
 
-func playerDied() -> void:
-    print("Player has died.")
-    # get_parent().queue_free() # This will remove the player from the scene
-    get_tree().reload_current_scene()
+@export var killDelay: float = 2
+@export var upwardForceOnDeath: float = 500
+
+var playerHasDied: bool = false
+
+func playerDied(doKillAnimation: bool) -> void:
+	if (playerHasDied): return
+	print("Player has died.")
+	playerHasDied = true
+	if (doKillAnimation):
+		var parentBody: CharacterBody2D = get_parent()
+		parentBody.get_node("CollisionShape2D").queue_free()
+		var animatedSprite: AnimatedSprite2D = parentBody.get_node("AnimatedSprite2D")
+		animatedSprite.z_index = 10000
+		animatedSprite.modulate.a = 0.5
+		parentBody.velocity.y = -upwardForceOnDeath
+		await get_tree().create_timer(killDelay).timeout
+	# get_parent().queue_free() # This will remove the player from the scene
+	get_tree().reload_current_scene()
